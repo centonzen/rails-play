6e66236279e309fc9ef3de44ee1a3365291f988d
Nick Centonze <Nick Centonze>
2016-09-03 01:04:13 -0500

    added Articles_controller#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,22 @@
 class ArticlesController < ApplicationController
 
-  def new
+  def show
+    @article = Article.find(params[:id])
+  end
 
+  def new
   end
 
   def create
     # render plain: params[:article].inspect
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
     @article.save
     redircet_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
