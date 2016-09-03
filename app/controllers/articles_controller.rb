1f0907422904489dcaaacc212211e7f29ad7e0ef
Nick Centonze <Nick Centonze>
2016-09-03 00:50:41 -0500

    stuff

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,10 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+    @article.save
+    redircet_to @article
   end
 
 end
